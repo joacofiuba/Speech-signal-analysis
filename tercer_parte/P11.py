af2cd292a1afeb934bc230a62044f3e5a8cb22c5
joaquin <jscardenas@fi.uba.ar>
2026-06-26 06:21:06 -0300

    p11 espectrograma Lapachos rapida

--- tercer_parte/P11.py
+++ tercer_parte/P11.py
@@ -58,3 +58,34 @@ fig.colorbar(im, ax=ax, label='Magnitud [dB]')
 
 plt.tight_layout()
 plt.show()
+
+
+# Espectrograma lapachos rapido
+
+archivo = "lapachos_rapido.wav"
+
+w_ancho = 2500 #aprox 56ms de ventana
+w = get_window(('hann'), w_ancho)
+
+x, sr = librosa.load(archivo, sr=fs) 
+#hop = salto de el sliding de la ventana, w_ancho//4 (división entera) implica un solapamiento entre cada iteración de un 25% de la ventana 
+SFT = ShortTimeFFT(win=w, hop=w_ancho//8, fs=fs,fft_mode='onesided', mfft=16384, dual_win=None, scale_to=None, phase_shift=0)
+Sx = SFT.stft(x)
+
+# Calcular la magnitud del espectrograma en decibeles (añadiendo un offset para evitar log(0))
+Sx_dB = 20 * np.log10(np.abs(Sx) + 1e-10)
+
+# Configurar y generar la gráfica
+fig, ax = plt.subplots(figsize=(10, 6))
+
+
+# SFT.extent(len(y)) retorna (t_min, t_max, f_min, f_max)
+im = ax.imshow(Sx_dB, origin='lower', aspect='auto', extent=SFT.extent(len(x)), vmax = 30, vmin = -30, cmap='viridis') # gráfico de la matriz
+ax.set_ylim(0, 7000)
+ax.set_title('Espectrograma - Banda Angosta - Lapachos rápida Interpolada')
+ax.set_xlabel('Tiempo [s]')
+ax.set_ylabel('Frecuencia [Hz]')
+fig.colorbar(im, ax=ax, label='Magnitud [dB]')
+
+plt.tight_layout()
+plt.show()
